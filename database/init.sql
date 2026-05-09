7c2e330abd07b4c3a99c8be56676fc93edb95819
AgustinPaternoster <agustinpaternoster@gmail.com>
2026-05-09 18:28:04 +0200

    feat: create db

---
+++ database/init.sql
@@ -0,0 +1,42 @@
+
+CREATE TABLE IF NOT EXISTS cliente (
+    id SERIAL PRIMARY KEY,
+    codigo_postal INTEGER,
+    provincia TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS producto (
+    id SERIAL PRIMARY KEY,
+    bloque_analitico TEXT,
+    categoria_h TEXT,
+    familia TEXT
+);
+
+CREATE TABLE IF NOT EXISTS ventas (
+    numero_factura INTEGER,
+    fecha DATE NOT NULL,
+    id_cliente INTEGER REFERENCES cliente(id),
+    id_producto INTEGER REFERENCES producto(id),
+    unidades INTEGER,
+    valores_h FLOAT, -- Valor económico de la transacción[cite: 2]
+    PRIMARY KEY (numero_factura, id_producto) -- Clave compuesta por factura y SKU
+);
+
+CREATE TABLE IF NOT EXISTS campanas (
+    id_campana SERIAL PRIMARY KEY,
+    campana TEXT,
+    fecha_inicio DATE,
+    fecha_fin DATE
+);
+
+-- CREATE TABLE IF NOT EXISTS alertas (
+--     id_alerta SERIAL PRIMARY KEY,
+--     fecha_generacion DATE DEFAULT CURRENT_DATE,
+--     id_cliente INTEGER REFERENCES cliente(id),
+--     familia_producto TEXT,
+--     motivo_alerta TEXT, -- Ej: "Riesgo abandono" o "Ventana captura"
+--     score_prioridad FLOAT, -- Basado en potencial e impacto
+--     prioridad_texto TEXT, -- Alta, Media, Baja
+--     canal_sugerido TEXT -- Delegado, Televenta o Marketing
+-- );
